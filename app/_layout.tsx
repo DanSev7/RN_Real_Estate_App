f794aacd350539e78982a86c7f75edb081afdc3d
DanSev7 <danielayele077@gmail.com>
2026-01-06 02:31:48 +0300

    Develop Sign In Screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -22,6 +22,10 @@ export default function RootLayout() {
   },[fontsLoaded])
 
   if (!fontsLoaded) return null;
-  
-  return <Stack />;
+
+  return <Stack 
+    screenOptions={{
+      headerShown: false,
+    }}
+  />;
 }
