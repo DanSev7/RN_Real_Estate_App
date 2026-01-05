eb678899ced88f17b2c654e9453bfd3a73556955
DanSev7 <danielayele077@gmail.com>
2026-01-05 23:59:25 +0300

    Setup Nativewind

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,19 +1,13 @@
-import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Link href="/sign-in">Go to sign-in page</Link>
-      <Link href="/explore">Go to explore page</Link>
-      <Link href="/profile">Go to profile page</Link>
-      <Link href="/properties/1">Go to property page</Link>
+    <View className="flex-1 justify-center items-center bg-white p-4">
+      <Text className="font-bold text-lg my-10 text-blue-500">Welcome to Restate</Text>
+      {/* <Link className="text-blue-500" href="/sign-in">Go to sign-in page</Link>
+      <Link className="text-blue-500" href="/explore">Go to explore page</Link>
+      <Link className="text-blue-500" href="/profile">Go to profile page</Link>
+      <Link className="text-blue-500" href="/properties/1">Go to property page</Link> */}
       
     </View>
   );
