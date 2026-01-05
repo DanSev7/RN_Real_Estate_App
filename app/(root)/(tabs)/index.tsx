bb51c3812d5fdf7bb7470b515fd9cc2c7cd06614
DanSev7 <danielayele077@gmail.com>
2026-01-06 02:12:45 +0300

    Setup Fonts and Assets

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,13 +1,14 @@
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
   return (
     <View className="flex-1 justify-center items-center bg-white p-4">
-      <Text className="font-bold text-lg my-10 text-blue-500">Welcome to Restate</Text>
-      {/* <Link className="text-blue-500" href="/sign-in">Go to sign-in page</Link>
-      <Link className="text-blue-500" href="/explore">Go to explore page</Link>
-      <Link className="text-blue-500" href="/profile">Go to profile page</Link>
-      <Link className="text-blue-500" href="/properties/1">Go to property page</Link> */}
+      <Text className="text-3xl font-rubikBold my-10">Welcome  to ReState</Text>
+      <Link className="text-blue-500 font-rubik" href="/sign-in">Go to sign-in page</Link>
+      <Link className="text-blue-500 font-rubik" href="/explore">Go to explore page</Link>
+      <Link className="text-blue-500 font-rubik" href="/profile">Go to profile page</Link>
+      <Link className="text-blue-500 font-rubik" href="/properties/1">Go to property page</Link> 
       
     </View>
   );
